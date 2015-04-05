5c737b4fc9158111532589a4da81150a311dd510
Piotr Fryga <piotr.fryga@allegro.pl>
2015-04-05 18:39:19 +0200

    get lowest iterator

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ var MongoClient = require('mongodb').MongoClient;
 var app = express();
 var port = 8082;
 
+var getLowestItemId = function (scores) {
+    var lowestValue = scores[0].score;
+    var lowestValueIterator = 0;
+
+    for (var i = 0; i < scores.length; i++) {
+        if (scores[i].score < lowestValue) {
+            lowestValue = scores[i].score;
+            lowestValueIterator = i;
+        }
+    }
+
+    return lowestValueIterator;
+}
 
 MongoClient.connect('mongodb://localhost:27017/school', function(err, db) {
     "use strict";
@@ -11,9 +24,15 @@ MongoClient.connect('mongodb://localhost:27017/school', function(err, db) {
     var students = db.collection('students');
     var coursor = students.find();
 
-    var i = 0;
+    var lowest;
+
     coursor.each(function (err, doc) {
-        console.log(i++);
+        if (doc == null) {
+            return 0;
+            db.close();
+        }
+
+        console.log(getLowestItemId(doc.scores));
     });
 
     app.listen(port, function () {
